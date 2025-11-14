a72d8ccc7858233e446db6827c06028a3a723383
Luiz Alberto Alves De Brito Junior <luizlaljr@gmail.com>
2025-11-14 11:07:14 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,18 @@
 import express, { Request, Response } from "express";
+import usersRoutes from "./routes/usersRoutes";
 
 const app = express();
+app.use(express.json());
+
+app.use(usersRoutes);
+
 const port = process.env.PORT || 3000;
 
+/*
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World 🌍");
 });
+*/
 
 app.listen(port, () => {
   console.log(`🚀 Servidor rodando em http://localhost:${port}`);
