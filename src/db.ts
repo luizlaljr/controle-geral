71e29d39824cb5d7be7dec266d07627d77553cb3
Luiz Alberto Alves De Brito Junior <luizlaljr@gmail.com>
2025-11-14 09:56:25 -0300

    Create db.ts

---
+++ src/db.ts
@@ -0,0 +1,14 @@
+import { Pool } from "pg";
+
+const pool = new Pool({
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  ssl: {
+    rejectUnauthorized: false, // Necessário no Northflank
+  },
+});
+
+export default pool;
